94e510c15c35ffede4d089b8c1acadfbb854c164
Clement.mahieux1 <clement_mahieux1@ens.univ-artois.fr>
2022-12-12 14:52:46 +0100

    commit du selecteur

--- app/Http/Controllers/OeuvreController.php
+++ app/Http/Controllers/OeuvreController.php
@@ -2,17 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Auteur;
 use App\Models\Oeuvre;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\Request;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Facades\DB;
 
 class OeuvreController extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
-    public function index() {
+    public function index(Request $request) {
         $oeuvres = Oeuvre::all();
-        return view('oeuvre.index', ['oeuvres' => $oeuvres]);
+        $auteurs = Auteur::select('nom')->get();
+        $param = $request->input('nom', 'all');
+        $oeuvres = DB::table('oeuvres')
+            ->join('auteurs','oeuvres_id','=','auteurs.id')
+            ->where('auteurs.nom', '=',$param)
+            ->select('oeuvres.*','auteurs.id','auteurs.nom as oeuvre_nom')
+            ->get();
+        return view('oeuvre.index', [
+            'oeuvres' => $oeuvres,
+            'param' => Auteur::all(),
+            'auteurs' => $auteurs
+        ]);
     }
 }
