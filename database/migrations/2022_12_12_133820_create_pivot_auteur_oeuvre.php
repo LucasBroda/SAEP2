a018359ad88490868fe7aee74f099855e4d318d2
Clement.mahieux1 <clement_mahieux1@ens.univ-artois.fr>
2022-12-12 17:12:49 +0100

    commit selecteur fonctionel avec debut de view show

---
+++ database/migrations/2022_12_12_133820_create_pivot_auteur_oeuvre.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('auteur_oeuvre', function (Blueprint $table) {
+            $table->foreignId('auteur_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('oeuvre_id')->constrained()->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('auteur_oeuvre');
+    }
+};
