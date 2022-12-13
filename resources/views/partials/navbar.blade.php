cf0baade44e01514fee5b441a8f012cce3dad147
lucas.broda <lucas_broda@ens.univ-artois.fr>
2022-12-13 14:09:39 +0100

    Suppression du lien vers les oeuvres

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -7,17 +7,18 @@
 
         <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
           <!-- <li><a href="#" class="nav-link px-2 text-secondary">Home</a></li> -->
-          <li><a href=" {{ route('oeuvre.index') }} " class="nav-link px-2 text-white">Oeuvres</a></li>
+
           <!--
           <li><a href="#" class="nav-link px-2 text-white">Pricing</a></li>
           <li><a href="#" class="nav-link px-2 text-white">FAQs</a></li>
           -->
           <li><a href=" {{ route('visiteur.info') }}" class="nav-link px-2 text-white">Info Visiteur</a></li>
+
         </ul>
 
         <div class="text-end">
-          <button type="button" class="btn btn-outline-light me-2">Login</button>
-          <button type="button" class="btn btn-warning">Sign-up</button>
+          <button type="button" class="btn btn-outline-light me-2"><a href=" {{ route('login') }}" class="nav-link px-2 text-white">Login</a></button>
+          <button type="button" class="btn btn-warning"><a href=" {{ route('register') }}" class="nav-link px-2 text-white">Sign-up</a></button>
         </div>
       </div>
     </div>
